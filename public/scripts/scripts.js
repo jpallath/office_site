83058b8c59b3ee335a49bcca1ea6c895487ccf9f
Jerry Pallath <jerry.pallath@mediaplanet.com>
2015-12-01 15:57:15 -0500

    this is kind of scary

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,7 +3,9 @@ $( document ).ready(function() {
     $( "a" ).click(function( event ) {
       $(".menu").css("width","0");
       $("a").css("display","none");
-      $(".toggle-nav").css("display","block");
+      $(".toggle-nav").css("display","inline-block")
+                      .css("position","fixed")
+                      .css("top","0");
  
     });
     
@@ -12,6 +14,14 @@ $( document ).ready(function() {
       $("a").css("display","block");
       $(".toggle-nav").css("display","none");
  
+    });
+    
+    $("main").click(function(event){
+      $(".menu").css("width","0");
+      $("a").css("display","none");
+      $(".toggle-nav").css("display","inline-block")
+                      .css("position","fixed")
+                      .css("top","0");
     });
  
 });
